45a0335c1d5cd6871a84738f24dd686a7fa7a793
Catrin Burrows <P12190552@GH582-011.lec-admin.dmu.ac.uk>
2014-01-29 10:42:27 +0000

    added min max function

--- maths.c
+++ maths.c
@@ -3,7 +3,7 @@ int main()
 {
 int a = 1;
 int b = 2;
-int c,d,e,f;
+int c,d,e,f, g, h, i, j, k;
 
 printf("%i\n", a+b);
 printf("%i\n", b-a);
@@ -15,5 +15,10 @@ scanf("%i %i %i %i", &c,&d,&e,&f);
 printf("\n %i is the average\n", (c+d+e+f)/4);
 
 printf("remainder of %i and %i is %i\n", a, b, a%b);
+
+printf("Enter in 5 Numbers:\n");
+scanf("%i %i %i %i %i", &g, &h, &i, &j, &k);
+if (g>h AND g>i AND g>j AND g>k)
+	printf("%i is the maximum", g)
 
 }
